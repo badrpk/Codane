35c668ac47adcf05678de1cddefcf6190e18cac2
Badar Uzaman <60453768+badrpk@users.noreply.github.com>
2026-09-15 02:33:31 +0500

    Route bare codane command to interactive TUI

---
+++ src/entry.cpp
@@ -0,0 +1,17 @@
+#include "codane/tui.hpp"
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+int codane_cli_main(int argc, char** argv);
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO)) {
+            return codane::run_tui(argv[0]);
+        }
+        return codane_cli_main(argc, argv);
+    }
+    return codane_cli_main(argc, argv);
+}
